9643469592a68b61a3df2419c86096f6c2ebff84
Yaron de Leeuw <me@jarondl.net>
2011-03-14 18:00:43 +0200

    survival works

--- PROG/survival.py
+++ PROG/survival.py
@@ -10,13 +10,13 @@ nodes = 200
 
 rho0 = scipy.zeros(200)
 rho0[nodes//2] = 1
-pylab.hold(False)
+pylab.hold(True)
 t = scipy.linspace(0,1000,100)
 
 
 A = sparsedl.NN_matrix(nodes)
 eigvals, eigvecs = linalg.eig(A)
-decr = scipy.dot(eigvals,rho0)  # rho0 in the eigenvector basis
+decr = scipy.dot(eigvecs.transpose(),rho0)  # rho0 in the eigenvector basis
 
 def survival1(t):
     return sparsedl.rho(t,rho0,A,nodes//2)
@@ -26,11 +26,13 @@ def survival2(t): #Using decomposition
 vecsurvival1 = pylab.np.vectorize(survival1)
 vecsurvival2 = pylab.np.vectorize(survival2)
 y1 = vecsurvival1(t)
-#y2 = vecsurvival2(t)
+y2 = vecsurvival2(t)
 #a,b = sparsedl.cvfit(sparsedl.strexp,t,y,[1,1])
 #m1 = stats.moment(y,1)
-pylab.semilogy(t,y1)#,t,y2)
-pylab.savefig('survival.png')
+pylab.semilogy(t,y1,label="Matrix exponent calculation")
+pylab.semilogy(t,y2,label="eigen vector decomposition", linestyle='--')
+pylab.legend()
+pylab.savefig('survival.eps')
 
 
 
