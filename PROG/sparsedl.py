86b77fceb6a656ff232fdd32b8471f826cd41952
Yaron de Leeuw <me@jarondl.net>
2011-05-29 20:13:49 +0300

    cont. pep8

--- PROG/sparsedl.py
+++ PROG/sparsedl.py
@@ -24,8 +24,9 @@ def NN_matrix(N, b=1, seed=None, **kwargs):
     matrix = numpy.zeros([N, N])
     for diag in range(1, b + 1):
         off_diag_values = lognormal_construction(N - diag, **kwargs)
-        matrix += numpy.diagflat(off_diag_values, diag) + numpy.diagflat(off_diag_values, - diag)
-    diag_values = matrix.sum(axis=1)  # the diagonal should contain minus the sum of rows
+        matrix += (numpy.diagflat(off_diag_values, diag) +
+                   numpy.diagflat(off_diag_values, - diag))
+    diag_values = matrix.sum(axis=1)  # the diagonal contains minus the sum of rows
     matrix -= numpy.diagflat(diag_values)
     return matrix
 
@@ -34,8 +35,8 @@ def lognormal_sparse_matrix(N, b=1, seed=None, **kwargs):
     """ Build a NxN matrix.
     Input: - N :  the size of the matrix
            - b : bandwidth
-           - **kwargs : the rest of the keyworded arguments are passed to the lognormal function,
-                            so you can define sigma and mu.
+           - **kwargs : the rest of the keyworded arguments are passed
+                    to the lognormal function, so you can define sigma and mu.
       log normal construction (e.g. mu=0, sigma=7)."""
     numpy.random.seed(seed)  # Used to create pseudo random values
     diag_data = lognormal_construction(N * b, **kwargs).reshape([b, N])
@@ -51,9 +52,11 @@ def rho(t, rho0, W, index=None):
     else:
         return scipy.dot(linalg.expm2(W * t), rho0)[index]
 
-#based on scipy 0.9's minpack
+
 def _general_function(params, xdata, ydata, function):
+    #based on scipy 0.9's minpack
     return function(xdata, *params) - ydata
+
 
 def cvfit(f, xdata, ydata, p0):
     """  Fit a curve. p0 is the initial guess (put ones if uncertain),
@@ -62,16 +65,22 @@ def cvfit(f, xdata, ydata, p0):
     res = optimize.leastsq(_general_function, p0, args=(xdata, ydata, f))
     return res[0]
 
+
 def strexp(x, a, b):
     return scipy.exp(-(x / a) ** b)
 
+
 def mean(x, y):
-    """  Returns the mean of a ditribution with x as location and y as value """
+    """  Returns the mean of a ditribution with x as location and y as value
+    """
     return scipy.sum(x * y) / scipy.sum(y)
 
+
 def var(x, y):
-    """  Returns the Variance of a ditribution with x as location and y as value """
+    """  Returns the Variance of a ditribution with x as location and y as value
+    """
     return mean(x ** 2, y) - mean(x, y) ** 2
+
 
 def initial(nodes):
     """  Some convient initial values, usefull for an interactive session.
@@ -81,6 +90,7 @@ def initial(nodes):
     A = NN_matrix(nodes)
     xcoord = scipy.linspace(-nodes / 2, (nodes / 2) - 1, nodes)
     return rho0, A, xcoord
+
 
 def zero_sum(mat, tol=1E-12):
     """  Change the diagonal elements of mat so that the sum of each row becomes 0
@@ -94,6 +104,7 @@ def zero_sum(mat, tol=1E-12):
             raise Exception("Failed to make sums zero, is the matrix symmetric?")
         return False
 
+
 def lognormal_construction(N, mu=0, sigma=1, **kwargs):
     """ Create log-normal distribution, with N elements, mu average and sigma width.
         The construction begins with a lineary spaced vector from 0 to 1. Then
@@ -104,6 +115,7 @@ def lognormal_construction(N, mu=0, sigma=1, **kwargs):
     rescaled_y = mu + (sigma * y)
     perm_y = scipy.random.permutation(rescaled_y)
     return scipy.exp(perm_y)
+
 
 def surv(eigen_values, times):
     """ Calculate survival probability by the sum of exponents equation"""
