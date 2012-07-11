f52516bc32b6a9740ed43445f9076606b339510d
Yaron de Leeuw <me@jarondl.net>
2012-07-11 16:08:43 +0300

    sent to kottos, start working on setup.py

--- DTS/dtsplot.py
+++ DTS/dtsplot.py
@@ -12,6 +12,8 @@ from matplotlib import rc
 import numpy as np
 import scipy as sp
 
+from sparsedl import cvfit
+import plotdl
 #import sparsedl
 #import plotdl
 #from geometry import Sample
@@ -20,8 +22,10 @@ import scipy as sp
 
 latex_width_inch = 3.4 ## (aps single column)
 latex_height_inch = latex_width_inch * (np.sqrt(5)-1.0)/2.0 # golden ratio
-rc('figure', figsize=[latex_width_inch*2, latex_height_inch*np.sqrt(3)])
-rc('legend', fontsize='smaller')
+#rc('figure', figsize=[latex_width_inch*2, latex_height_inch*np.sqrt(3)])
+rc('figure', figsize=[latex_width_inch, latex_height_inch])
+#rc('legend', fontsize='smaller')
+rc('legend', fontsize=7)
 rc('xtick', labelsize='smaller')
 rc('ytick', labelsize='smaller')
 
@@ -41,7 +45,12 @@ debug = logger.debug
 
 #RMS = lambda sig : np.sqrt( -np.expm1(-2*sig)/sig)
 RMS = lambda sig : np.sqrt( -np.expm1(-2*sig)/(2*sig))
-gs_ERH = lambda nc : lambda sig, b : ((1+nc/(2*b))*np.exp(-nc*sig/(2*b)) - np.exp(-2*sig))/(-np.expm1(-2*sig))
+#RMS = lambda sig : np.sqrt( -np.expm1(-4*sig)/(4*sig))
+#gs_ERH = lambda nc : lambda sig, b : ((1+nc/(2*b))*np.exp(-nc*sig/(2*b)) - np.exp(-2*sig))/(-np.expm1(-2*sig))
+# with \sigma/2
+#gs_ERH = lambda nc : lambda sig, b : ((1+nc/(2*b))*np.exp(-nc*sig/(4*b)) - np.exp(-sig))/(-np.expm1(-sig))
+#gs_ERH = lambda nc : lambda sig, b : ((1+nc*sig/(4*b))*np.exp(-nc*sig/(4*b)) - np.exp(-sig))/(-np.expm1(-sig))
+gs_ERH = lambda nc : lambda sig, b : ((1+nc*sig/(b))*np.exp(-nc*sig/(b)) - np.exp(-4*sig))/(-np.expm1(-4*sig))
 gs_ERH2 = gs_ERH(2)
 
 
@@ -69,9 +78,23 @@ def plot_D():
         #pylab.plot((nf[n:n+5, 1]),  nf[n:n+5,6]/ nf[n:n+5,0]**2.5, ls =ls)
     pylab.xlim(0,6)
     pylab.xlabel(r"$\sigma$")
-    pylab.ylabel("$D_1$ - scaled")
-    pylab.legend()
+    pylab.ylabel("$D_1/D_0$")
+    pylab.legend(loc="best")
 
+def plot_expected_gs():
+    f = read_data()
+    #f = f
+    #sig = f[:,1]
+    sig = np.linspace(1E-4,6)
+#    gs = gs_ERH(2)(sig, f[:,0])
+    for (b, (marker, mec)) in zip((10,20,40,100) , (('o','b'), ('s','r'), ('D', 'g'), ('.','k'))) :
+        
+        pylab.plot(sig, gs_ERH(2)(sig, b), color=mec, label="$b={0}$".format(b))
+
+    #pylab.xlim(0,40)
+    pylab.xlabel(r"$\sigma$")
+    pylab.ylabel("$g_s$")
+    pylab.legend(loc="best")
 
 def plot_D2():
     f = read_data()
@@ -85,10 +108,11 @@ def plot_D2():
         #pylab.plot((nf[n:n+5, 1]),  nf[n:n+5,6]/ nf[n:n+5,0]**2.5, ls =ls)
     pylab.xlim(0,6)
     pylab.xlabel(r"$\sigma$")
-    pylab.ylabel("$D_2$ - scaled)")
-    pylab.legend()
+    pylab.ylabel("$D_2/D_0$")
+    pylab.legend(loc="best")
 
 def plot_D2_vs_gs():
+    # New ! 
     f = read_data()
     #f = f
     sig = f[:,1]
@@ -100,11 +124,41 @@ def plot_D2_vs_gs():
         #pylab.plot((nf[n:n+5, 1]),  nf[n:n+5,6]/ nf[n:n+5,0]**2.5, ls =ls)
     #pylab.xlim(0,6)
     xspace1 = np.linspace(0.65,0.79)
-    pylab.plot( xspace1, xspace1, "k--")
+    #pylab.plot( xspace1, xspace1, "k--")
     xspace2 = np.linspace(0.65,0.9)
-    pylab.plot( xspace2, 0.5*xspace2+0.33, ":", color='0.5')
+    #pylab.plot( xspace2, 0.5*xspace2+0.33, ":", color='0.5')
     pylab.xlabel(r"$g_s(\sigma)$")
-    pylab.ylabel("$D_2$ - scaled")
+    pylab.ylabel("$D_2/D_0$ - scaled")
+    pylab.legend(loc="best")
+
+def plot_D2_vs_gs_loglog():
+    # New ! now with loglog scale
+    f = read_data()
+    #f = f
+    sig = f[:,1]
+    D2_scaled = f[:,6] / ( RMS( f[:,1] ) * f[:,0]**2.5 )
+    D2_log = np.log(D2_scaled)
+    gs_log = np.log(gs_ERH(2)(sig, f[:,0]))
+
+    for (n, (marker, mec)) in zip((0,5,10) , (('o','b'), ('s','r'), ('D', 'g'))) :
+        pylab.plot(gs_log[n:n+5] ,  D2_log[n:n+5], ' ', mfc='none',
+                marker=marker, mec=mec, label=" b = {0}".format(f[n,0]))
+        xspace1 = np.linspace(gs_log[n:n+5].min(), gs_log[n:n+5].max())
+        p = np.polyfit(gs_log[n:n+5], D2_log[n:n+5], 1)
+
+        p1 = np.poly1d(p)
+        pylab.plot(xspace1, p1(xspace1), color=mec, label="$y={p[1]:.2}+ {p[0]:.02}x$".format(p=p))
+
+
+        #pylab.plot(sig[n:n+5] ,  gs_ERH(2)(sig[n:n+5], f[n,0]), '--', color= mec)
+        #pylab.plot((nf[n:n+5, 1]),  nf[n:n+5,6]/ nf[n:n+5,0]**2.5, ls =ls)
+    #pylab.xlim(0,6)
+
+    # dilute the ticks
+    pylab.xticks(pylab.xticks()[0][::2])
+    
+    pylab.xlabel(r"$\log\  g_s(\sigma)$")
+    pylab.ylabel("$\log \ D_2/D_0$")
     pylab.legend(loc="best")
 
 
@@ -121,7 +175,7 @@ def plot_v2():
     pylab.xlim(0,6)
     pylab.xlabel(r"$\sigma$")
     pylab.ylabel("$V^2$ - scaled")
-    pylab.legend()
+    pylab.legend(loc="best")
 
 
 
@@ -136,14 +190,17 @@ def plot_tball():
     pylab.xlim(0,6)
     pylab.xlabel(r"$\sigma$")
     pylab.ylabel(r"$t_{bal}$ - scaled")
-    pylab.legend()
+    pylab.legend(loc="best")
 
 
 def plots_for_dtstex():
-    pylab.close()
+
+    f = pylab.gcf()
     pylab.clf()
     plot_D()
+    plotdl.tight_layout(f, pad=0.4)
     pylab.savefig("new/D1.pdf")
+    #plotdl.save_fig(f, "new/D1.pdf")
     pylab.clf()
     plot_D2()
     pylab.savefig("new/D2.pdf")
@@ -157,7 +214,13 @@ def plots_for_dtstex():
     plot_tball()
     pylab.savefig("new/tball.pdf")
     pylab.clf()
-    pylab.close()
+    plot_D2_vs_gs_loglog()
+    pylab.savefig("new/D2_vs_gs_loglog.pdf")
+    pylab.clf()
+
+    plot_expected_gs()
+    pylab.savefig("new/expected_gs.pdf")
+    pylab.clf()
 
 
 
