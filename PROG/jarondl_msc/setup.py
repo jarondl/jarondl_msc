2753cb8f34ba081f5dfb948134a24867c42a8b7a
Yaron de Leeuw <me@jarondl.net>
2013-08-20 21:53:15 +0300

    minor change to setup.py

--- PROG/jarondl_msc/setup.py
+++ PROG/jarondl_msc/setup.py
@@ -3,9 +3,9 @@
 from setuptools import setup
 
 setup(name = "jarondl_msc",
-      version = '1.2',
+      version = '1.3',
       description = "All of the code needed to do the numerics and plots of my MSc",
       author = "Yaron de Leeuw",
-      packages=['jarondl_msc']
+      packages=['jarondl_msc', 'jarondl_msc.libdl']
      )
 
